2d7db0adde6a8580de9205eb4e97167a9039acf8
BokarevDmitry <dbokarevv@gmail.com>
2019-11-20 23:16:06 +0300

    Scripted auto-commit on change (2019-11-20 23:16:06) by gitwatch.sh

--- src/main/java/bokarev/Main.java
+++ src/main/java/bokarev/Main.java
@@ -1,8 +1,17 @@
 package bokarev;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
 
 //import static bokarev.StoreActor.Msg.GREET;
 
@@ -13,8 +22,17 @@ public class Main {
         ActorSystem system = ActorSystem.create("Actor-System");
 
         final Http http = Http.get(system);
-        fina;
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        MainHttp instance = new MainHttp(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
 
+        System.out.println("Server online at http://localhost:8080");
 
 
 
