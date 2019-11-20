64eda9b57c528b517472a1cc4c578cb9a39283bd
BokarevDmitry <dbokarevv@gmail.com>
2019-11-20 20:59:43 +0300

    Scripted auto-commit on change (2019-11-20 20:59:43) by gitwatch.sh

--- src/main/java/bokarev/TestPasserActor.java
+++ src/main/java/bokarev/TestPasserActor.java
@@ -1,4 +1,68 @@
 package bokarev;
 
-public class TestPasserActor {
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import java.util.ArrayList;
+
+
+public class TestPasserAcor extends AbstractActor {
+
+    private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    private ArrayList<Integer> testResults;
+
+    public TestPasserAcor() {
+        this.testResults = new ArrayList<>();
+    }
+
+    public static Props props() {
+        return Props.create(TestPasserAcor.class);
+    }
+
+    public static final class TestResultClass {
+        int testResult;
+
+        public TestResultClass(int testResult) {
+            this.testResult = testResult;
+        }
+    }
+
+    public static final class getTestsClass {
+        int packageID;
+
+        public getTestsClass(int packageID) {
+            this.packageID = packageID;
+        }
+    }
+
+    @Override
+    public void preStart() {
+        log.info("Starting StorageActor {}", this);
+    }
+
+    @Override
+    public void postStop() {
+        log.info("Stopping StorageActor {}", this);
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(TestResultClass.class, r -> {
+                    log.info("Received test result message");
+                    this.testResults.add(r.testResult);
+                })
+
+                .match(getTestsClass.class, r -> {
+                    log.info("Received Get Test Request message for package " + r.packageID);
+                    log.info("Test results: " + this.testResults);
+                })
+
+                .build();
+    }
 }
+
