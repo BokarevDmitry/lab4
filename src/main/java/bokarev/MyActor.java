5ff639ed0403552c6ed29dcf84b60bcece58b576
BokarevDmitry <dbokarevv@gmail.com>
2019-11-20 19:11:48 +0300

    Scripted auto-commit on change (2019-11-20 19:11:48) by gitwatch.sh

--- src/main/java/bokarev/MyActor.java
+++ src/main/java/bokarev/MyActor.java
@@ -1,40 +1,25 @@
 package bokarev;
 
 import akka.actor.AbstractActor;
-import akka.japi.pf.ReceiveBuilder;
-import java.util.HashMap;
-import java.util.Map;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 
-/*public class StoreActor extends AbstractActor {
-    private Map<String, String> store = new HashMap<>();
+public class MyActor extends AbstractActor {
+
+    private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
 
     @Override
-    public Receive createReceive() {
-        return ReceiveBuilder.create()
-                .match(StoreMessage.class, m -> {
-                    store.put(m.getKey(), m.getValue());
-                    System.out.println("received msg " + m.toString());
-                })
-                .match(GetMessage.class, req -> sender().tell(
-                        new StoreMessage(req.getKey(), store.get(req.getKey())), self())
-                ).build();
+    public void postStop() {
+        log.info("Stopping actor {}", this);
     }
-}*/
 
-
-public class StoreActor extends AbstractActor {
-    public static enum Msg {
-        GREET, DONE
-    }
-    @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .matchEquals(Msg.GREET, m -> {
-                    System.out.println("Hello World!");
-                    sender().tell(Msg.DONE, self());
+                .matchEquals("printit", p -> {
+                    System.out.println("The address of this actor is: " + getSelf());
+                    getSender().tell("Got Message", getSelf());
                 })
                 .build();
     }
 }
-
 
