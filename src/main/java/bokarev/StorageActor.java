2edc63684b0b21968ed60c9e595243f7eee3ab0f
BokarevDmitry <dbokarevv@gmail.com>
2019-11-20 22:29:37 +0300

    Scripted auto-commit on change (2019-11-20 22:29:37) by gitwatch.sh

--- src/main/java/bokarev/StorageActor.java
+++ src/main/java/bokarev/StorageActor.java
@@ -64,7 +64,7 @@ public class StorageActor extends AbstractActor {
 
                 .match(getTestsClass.class, r -> {
                     log.info("Received Get Test Request message for package " + r.packageID);
-                    log.info("Test results: " + this.testResults);
+                    log.info("Test results: " + this.testResults.get(0));
                 })
 
                 .build();
