68ef02897e91f6a67b7d57f0a1c89f420b803c8e
BokarevDmitry <dbokarevv@gmail.com>
2019-11-20 23:48:58 +0300

    Scripted auto-commit on change (2019-11-20 23:48:58) by gitwatch.sh

--- src/main/java/bokarev/MainHttp.java
+++ src/main/java/bokarev/MainHttp.java
@@ -18,7 +18,7 @@ import akka.stream.javadsl.Flow;
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.Future;
-import scala.compat.*;
+import scala.compat.java8.OptionConverters;
 
 
 //import static bokarev.StoreActor.Msg.GREET;
