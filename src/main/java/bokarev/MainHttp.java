2101dd833533c77d640a9f8313b12ce8748cd9b4
BokarevDmitry <dbokarevv@gmail.com>
2019-11-21 00:03:34 +0300

    Scripted auto-commit on change (2019-11-21 00:03:34) by gitwatch.sh

--- src/main/java/bokarev/MainHttp.java
+++ src/main/java/bokarev/MainHttp.java
@@ -1,47 +1,110 @@
 package bokarev;
 
 import akka.NotUsed;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
+import java.io.IOException;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Future;
+//import scala.compat.java8.OptionConverters;
+import scala.compat.Platform.*;
+
+
+//import static bokarev.StoreActor.Msg.GREET;
+
 
 public class MainHttp extends AllDirectives {
+    public static void main(String[] args) throws Exception, InterruptedException, IOException {
 
-    public static void main(String[] args) throws Exception {
-        // boot up server using the route as defined below
-        ActorSystem system = ActorSystem.create("routes");
+        ActorSystem system = ActorSystem.create("Actor-System");
 
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-
-        //In order to access all directives we need an instance where the routes are define.
-        MainHttp app = new MainHttp();
-
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
-        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
-                ConnectHttp.toHost("localhost", 8080), materializer);
+        MainHttp instance = new MainHttp();
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
 
         System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-        System.in.read(); // let it run until user presses return
-
+        System.in.read();
         binding
-                .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
-                .thenAccept(unbound -> system.terminate()); // and shutdown when done
-    }
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
 
-    private Route createRoute() {
+
+
+
+
+
+
+
+        /*ActorRef storageActorRef = system.actorOf (StorageActor.props(), "Storage-Actor");
+
+
+        //storageActorRef.tell(new RouterActor.TestResult(1), ActorRef.noSender());
+        //storageActorRef.tell(new RouterActor.TestResult(7), ActorRef.noSender());
+        //storageActorRef.tell(new RouterActor.TestResult(2), ActorRef.noSender());
+
+        //storageActorRef.tell(new RouterActor.getTestsClass(11), ActorRef.noSender());
+
+
+        Object[] params = {2,1};
+        ActorRef testPasserActorRef = system.actorOf(TestPasserActor.props(), "TestPasser-Actor");
+        testPasserActorRef.tell(new TestPasserActor.Test(
+                11, "var divideFn = function(a,b) {return a/b}",
+                "divideFn", "test1", 2.0, params), storageActorRef);
+
+
+        Thread.sleep(1000);
+        storageActorRef.tell(new StorageActor.getTestsClass(11), ActorRef.noSender());
+        */
+    }
+    private Route createRoute(ActorSystem system) {
         return concat(
                 path("hello", () ->
                         get(() ->
                                 complete("<h1>Say hello to akka-http</h1>"))));
     }
+   /* private Route createRoute(ActorSystem system) {
+        return route(
+                path("semaphore", () ->
+                        route(
+                                get( () -> {
+                                    Future<Object> result = Patterns.ask(testPackageActor,
+                                            SemaphoreActor.makeRequest(), 5000);
+                                    return completeOKWithFuture(result, Jackson.marshaller());
+                                }))),
+                path("test", () ->
+                        route(
+                                post(() ->
+                                        entity(Jackson.unmarshaller(TestPackageMsg.class), msg -> {
+                                            testPackageActor.tell(msg, ActorRef.noSender());
+                                            return complete("Test started!");
+                                        })))),
+                path("put", () ->
+                        get(() ->
+                                parameter("key", (key) ->
+                                        parameter("value", (value) ->
+                                        {
+                                            storeActor.tell(new StoreActor.StoreMessage(key, value), ActorRef.noSender());
+                                            return complete("value saved to store ! key=" + key + " value=" + value);
+                                        }))));
+    }*/
+
 }
