55079fd6b042cceefab54a3a40dcaaa3f059d4b5
BokarevDmitry <dbokarevv@gmail.com>
2019-11-22 22:39:59 +0300

    Scripted auto-commit on change (2019-11-22 22:39:59) by gitwatch.sh

--- src/main/java/bokarev/MainHttp.java
+++ src/main/java/bokarev/MainHttp.java
@@ -48,7 +48,6 @@ public class MainHttp extends AllDirectives {
                         route(
                                 get(() ->
                                     parameter("packageId", packageId -> {
-                                        System.out.println(packageId);
                                         Future<Object> future = Patterns.ask(routerActor, new Classes.TestGetter(Integer.parseInt(packageId)), 5000);
                                         return completeOKWithFuture(future, Jackson.marshaller());
                                     })
